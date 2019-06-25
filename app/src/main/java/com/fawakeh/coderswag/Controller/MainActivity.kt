1d6d196df616f814372b63b3bb8520f0f6aac1a6
rHachem <18rawad@gmail.com>
2019-06-25 13:51:20 +0300

    use list view with custom adapter + use of view holder

--- app/src/main/java/com/fawakeh/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/fawakeh/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.fawakeh.coderswag.Adapters.CategoryAdapter
 import com.fawakeh.coderswag.Model.Category
 import com.fawakeh.coderswag.R
 import com.fawakeh.coderswag.Services.DataService
@@ -10,13 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
  class MainActivity : AppCompatActivity() {
 
-     lateinit var adapter: ArrayAdapter<Category>
+     lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
     }
